37dd09b2dc5ecfb7cc91d9b02670499e43ee4823
Jimmy <jpham197@gmail.com>
2018-06-23 15:04:20 -0400

    installed sqlite3

--- db/api.js
+++ db/api.js
@@ -1,6 +1,16 @@
 const express = require(`express`);
+const sql = require(`sqlite3`).verbose();
 const app = express();
 
 const server = app.listen(3000, () => {
     console.log(`server listening..`);
 });
+
+let db = new sqlite3.Database(':memory:', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+  });
+
+  db.close();
